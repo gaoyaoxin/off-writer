359fa9a89c9664e2d19e8cf0ff090fafd4afa31d
NickolasGame <nickolaskuryliuk@gmail.com>
2016-10-15 20:26:28 +0300

    add a hover effect for cards
    if mouse is hover on card paragraph color changes to lightblue

--- website/js/ui.js
+++ website/js/ui.js
@@ -1,1 +1,94 @@
 $(".dropdown-content").dropdown();
+
+$(document).ready(function() {
+    $(".card-item-1").hover(function() {
+        $(".card-item-1-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-1-fix").css("color", "black");
+    });
+
+    $(".card-item-2").hover(function() {
+        $(".card-item-2-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-2-fix").css("color", "black");
+    });
+
+    $(".card-item-3").hover(function() {
+        $(".card-item-3-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-3-fix").css("color", "black");
+    });
+
+    $(".card-item-4").hover(function() {
+        $(".card-item-4-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-4-fix").css("color", "black");
+    });
+
+    $(".card-item-5").hover(function() {
+        $(".card-item-5-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-5-fix").css("color", "black");
+    });
+
+    $(".card-item-6").hover(function() {
+        $(".card-item-6-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-6-fix").css("color", "black");
+    });
+
+    $(".card-item-7").hover(function() {
+        $(".card-item-7-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-7-fix").css("color", "black");
+    });
+
+    $(".card-item-8").hover(function() {
+        $(".card-item-8-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-8-fix").css("color", "black");
+    });
+
+    $(".card-item-9").hover(function() {
+        $(".card-item-9-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-9-fix").css("color", "black");
+    });
+
+    $(".card-item-10").hover(function() {
+        $(".card-item-10-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-10-fix").css("color", "black");
+    });
+
+    $(".card-item-11").hover(function() {
+        $(".card-item-11-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-11-fix").css("color", "black");
+    });
+
+    $(".card-item-12").hover(function() {
+        $(".card-item-12-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-12-fix").css("color", "black");
+    });
+
+    $(".card-item-13").hover(function() {
+        $(".card-item-13-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-13-fix").css("color", "black");
+    });
+
+    $(".card-item-14").hover(function() {
+        $(".card-item-14-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-14-fix").css("color", "black");
+    });
+
+    $(".card-item-15").hover(function() {
+        $(".card-item-15-fix").css("color", "#039be5");
+    }, function() {
+        $(".card-item-15-fix").css("color", "black");
+    });
+
+});
